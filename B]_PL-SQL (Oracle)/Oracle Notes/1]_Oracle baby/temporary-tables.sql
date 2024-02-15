d1dc5ef3fc6334db42a8b37d2c4713e71faf0ad2
ayon_ssp <ayon.ssp@gmail.com>
2024-02-15 17:09:25 +0530

    feat: tables updated

--- B]_PL-SQL (Oracle)/Oracle Notes/1]_Oracle baby/temporary-tables.sql
+++ B]_PL-SQL (Oracle)/Oracle Notes/1]_Oracle baby/temporary-tables.sql
@@ -1,21 +1,24 @@
+-- In Oracle Database, global temporary tables are permanent objects whose data are stored on disk and automatically deleted at the end of a session or transaction. In addition, global temporary tables are visible to all sessions currently connected to the database.
+
 
 -- 0. Global Temporary Tables:
+
+
 CREATE GLOBAL TEMPORARY TABLE temp1(
     id INT,
     description VARCHAR2(100)
 ) ON COMMIT DELETE ROWS;
 
+-- Transaction specific global temporary table
+BEGIN
+    INSERT INTO temp1(id,description)
+    VALUES(1,'Transaction specific global temp table');
 
-INSERT INTO temp1(id,description)
-VALUES(1,'Transaction specific global temp table');
+    SELECT id, description 
+    FROM temp1;
+END;
 
-SELECT id, description 
-FROM temp1;
-
-
-COMMIT;
-
-DROP TABLE temp1;
+COMMENT;
 
 
 -- 1.Inline Temporary Tables (WITH clause):
